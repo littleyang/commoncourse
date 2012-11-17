1f700c14a68f7c0fddd269f4b4e9fe9a08abc1ac
littleyang <littleyangqh@gmail.com>
2012-11-17 15:43:12 +0800

    start a new project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Commoncourse::Application.config.secret_token = '5c2faf511243ed6a0f5cd701eb9af65a164030c4b21f73e6cddf9bf5e383ab7b3dd64339aeeb561f5eca881f86cb15a77be1f7fcd049c1a42bee9e1363c5d02c'
